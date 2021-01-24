7ce5dc54a2addda6984eb344cfcca58ac2f1cc1c
Mateus Galasso <mateusgalasso@yahoo.com.br>
2021-01-24 17:50:08 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,7 @@ use Illuminate\Support\Facades\Redis;
 
 Route::get('/', function () {
     try{
-        dump('maisteste');
-        Cache::put('key', 'value2');
+        Cache::put('key', 'value');
         dump(Cache::get('key'));
 //        $redis=Redis::connect('127.0.0.1',3306);
         return response('redis working');
